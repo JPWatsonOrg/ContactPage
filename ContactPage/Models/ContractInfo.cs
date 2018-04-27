895ecb14d71934a1c92fb1460252aa0df88ba489
Jason Watson <jason.watson@gmail.com>
2018-04-27 03:53:06 -0500

    Started some work on validation

--- ContactPage/Models/ContractInfo.cs
+++ ContactPage/Models/ContractInfo.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContactPage.Models
 {
@@ -7,8 +8,14 @@ namespace ContactPage.Models
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
+        [Required(ErrorMessage = "You must provide a phone number")]
+        [Display(Name = "Home Phone")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
         public string phoneNumber { get; set; }
+        [Required]
         public string emailAddress { get; set; }
     }
 }
